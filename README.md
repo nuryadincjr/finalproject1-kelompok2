d646a18cae1ed2df5a61cf3fa0653bd932f10fad
nuryadincjr <nuryadin.cjr@gmail.com>
2021-10-27 20:55:16 +0700

    Layouting: make list item for recyclerView, make splash screen Activity with handler.

--- README.md
+++ README.md
@@ -4,4 +4,4 @@ Studi independen Kampus Merdeka
 
 ## Screenshots
 
-<img src="https://github.com/nuryadincjr/finalproject1-kelompok2/blob/main/img/1.png" width="233" height="483">
+<img src="https://github.com/nuryadincjr/finalproject1-kelompok2/blob/main/img/1.png" width="233" height="483">  <img src="https://github.com/nuryadincjr/finalproject1-kelompok2/blob/main/img/2.png" width="233" height="483">
---
+++ app/src/main/java/com/nuryadincjr/todolist/activity/SplashScreenActivity.java
@@ -0,0 +1,32 @@
+package com.nuryadincjr.todolist.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+import com.nuryadincjr.todolist.MainActivity;
+import com.nuryadincjr.todolist.R;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        getSupportActionBar().hide();
+
+        transition();
+    }
+
+    private void transition() {
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(() -> {
+            startActivity(new Intent(this, MainActivity.class));
+            finish();
+        }, 3000);
+    }
+}
